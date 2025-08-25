ce7047afe0db7bee881ff049cf0a7db5186e430b
Ben4 <benymany511@gmail.com>
2025-08-25 19:35:41 +0300

    third commit poll

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,73 +1,24 @@
-const pollTitle = document.querySelector('.poll__title');
-const pollAnswers = document.querySelector('.poll__answers');
-
-
 let xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
-
+xhr.open(`GET`, `https://students.netoservices.ru/nestjs-backend/poll`);
 xhr.responseType = 'json';
-
-xhr.onload = () => {
-
-
-    for(let key in xhr.response.data.title) {
-
-        console.log(xhr.response.data.title[key]);
-
-        pollTitle.textContent += xhr.response.data.title[key];
-    }
-
-    for(let key in xhr.response.data.answers) {
-
-        pollAnswers.innerHTML += `
-        <button class="poll__answer">
-        ${xhr.response.data.answers[key]}
-        </button>`
-
-        const pollAnawer = Array.from(document.querySelectorAll('.poll__answer'));
-
-        pollAnawer.forEach((answer) => {
-                 answer.addEventListener("click", (event) => sendPost(event, xhr.response))
-            });
-    }
-    
-}
-
 xhr.send();
- 
-function sendPost(event, data) {
 
-const btnText = event.target.textContent;
-const answerIndex = data.data.answers.indexOf(btnText.trim());
-
-
-const xhrPost = new XMLHttpRequest;
-xhrPost.open( 'POST', 'https://students.netoservices.ru/nestjs-backend/poll' );
-xhrPost.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
-
-xhrPost.onload = () => {
-    const results = JSON.parse(xhrPost.response).stat;
-    let sum = 0;
-    let tempString = '';
-
-    results.forEach((result) => {
-        sum += result.votes;
+xhr.addEventListener(`readystatechange`, () => {
+  if (xhr.readyState === xhr.DONE) {
+    const request = xhr.response;
+    const pullTitle = document.querySelector(`.poll__title`);
+    const pullAnswers = document.querySelector(`.poll__answers`);
+    pullTitle.textContent = request.data.title;
+    request.data.answers.forEach(button => {
+      pullAnswers.appendChild(creatAnswer(button));
     });
+  }
+})
 
-    results.forEach((result)=> {
-        tempString += `<li>${result.answer}:${(result.votes / sum ) * 100}%</li>`;
-    });
-
-    pollAnswers.innerHTML += `
-    <ul>
-    ${tempString}
-    </ul>
-    `
-}
-
-
-
-xhrPost.send(`vote=${data.id}&answer=${answerIndex}`);
-
+function creatAnswer(question) {
+  const answer = document.createElement(`button`);
+  answer.setAttribute(`class`, `poll__answer`);
+  answer.textContent = question;
+  answer.addEventListener(`click`, () => alert(`Спасибо, ваш голос засчитан!`));
+  return answer
 }
